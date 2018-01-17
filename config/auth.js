1eceb921043cac0f6aa79823557438f1a44a0491
a7mad3akef <a7mad3akef@gmail.com>
2018-01-17 14:11:13 +0200

    first push request

---
+++ config/auth.js
@@ -0,0 +1,27 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : '379060449188375', // your App ID
+        'clientSecret'    : '2b5f8fb67513fce202cb868631e89865', // your App Secret
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'your-consumer-key-here',
+        'consumerSecret'     : 'your-client-secret-here',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : '793005397109-72n125b0t8fp5as6at64fbragr6ogk1f.apps.googleusercontent.com',
+        'clientSecret'     : 'XFcCgiWLHvuNsmLIzBALLFe2',
+        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://name:password@ds113586.mlab.com:13586/oauth-rix' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
