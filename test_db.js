5639eae84747c681956e912f438869feed9925b2
a7mad3akef <a7mad3akef@gmail.com>
2018-01-17 20:09:48 +0200

    try to query the db

--- test_db.js
+++ test_db.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var databaseName = 'discogs'
-var url = 'mongodb://' + 'adminrole' + ':' + 'susuSUSU1234!%40%23%24' + '@127.0.0.1:' + '27017' + '/' + databaseName;
+var url = 'mongodb://' + 'discogsadmin' + ':' + 'discogsa1234' + '@127.0.0.1:' + '27017' + '/' + databaseName;
 
 
 MongoClient.connect(url, function(err, db) {
