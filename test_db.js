f7ab9ca62eef52fe7773d3592678b18dd2fbc24e
a7mad3akef <a7mad3akef@gmail.com>
2018-01-18 18:03:13 +0200

    try to find song by id

--- test_db.js
+++ test_db.js
@@ -4,15 +4,15 @@ var databaseName = 'discogs'
 var url = 'mongodb://' + 'akef' + ':' + 'akef1234' + '@127.0.0.1:' + '27017' + '/' + databaseName;
 
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var query = { title:'Östermalm' };
-  db.collection("songs").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var query = { title:'Östermalm' };
+//   db.collection("songs").find(query).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
 
 function get_song_by_id (theId, callback){
     MongoClient.connect(url, function(err, db) {
