b7c733a4bf500eeb949b883858f1e635441a155c
a7mad3akef <a7mad3akef@gmail.com>
2018-01-18 21:41:27 +0200

    add song to events

--- test_db.js
+++ test_db.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var databaseName = 'discogs'
-var url = 'mongodb://' + 'akef' + ':' + 'akef1234' + '@127.0.0.1:' + '27017' + '/' + databaseName;
+var url = 'mongodb://' + 'kofa' + ':' + 'kofa' + '@127.0.0.1:' + '27017' + '/' + databaseName;
 
 
 // MongoClient.connect(url, function(err, db) {
