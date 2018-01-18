a14d5ac998860b5cbe2eeac04d6d1bfeba653479
a7mad3akef <a7mad3akef@gmail.com>
2018-01-18 21:14:00 +0200

    try to find song by id

--- test_db.js
+++ test_db.js
@@ -4,15 +4,15 @@ var databaseName = 'discogs'
 var url = 'mongodb://' + 'akef' + ':' + 'akef1234' + '@127.0.0.1:' + '27017' + '/' + databaseName;
 
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var query = { title:'Östermalm' };
-  db.collection("songs").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var query = { title:'Östermalm' };
+//   db.collection("songs").find(query).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
 
 function get_song_by_id (theId, callback){
     MongoClient.connect(url, function(err, db) {
@@ -27,6 +27,6 @@ function get_song_by_id (theId, callback){
       });  
 }
 
-// get_song_by_id('59fd33f752f630421ccdf2a8',function(result){
-//     console.log(result)
-// })
+get_song_by_id('5a16b3e1a5b94b1626140002',function(result){
+    console.log(result)
+})
