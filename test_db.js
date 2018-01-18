a397b358c7eefc3a982fb1ba65d69704a0612ef6
a7mad3akef <a7mad3akef@gmail.com>
2018-01-18 21:37:53 +0200

    add song to events

--- test_db.js
+++ test_db.js
@@ -27,6 +27,31 @@ function get_song_by_id (theId, callback){
       });  
 }
 
-get_song_by_id('5a16b3e1a5b94b1626140002',function(result){
+function add_song_to_events(theId, callback){
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var query = { _id: ObjectId(theId) };
+    db.collection("songs").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      delete result[0]._id
+      result[0].user = "root"
+      result[0].song_id = theId
+      result[0].likes = []
+      result[0].dislikes = []
+      result[0].comments = []
+      result[0].pushes = []
+      db.collection("events").insertOne(result[0], function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+      callback(result)
+    });
+    
+   
+  });
+}
+
+add_song_to_events('5a16b3e1a5b94b1626140002',function(result){
     console.log(result)
 })
