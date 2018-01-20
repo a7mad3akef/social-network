08536e820196e9134b02225c0d536a0ed901e490
a7mad3akef <a7mad3akef@gmail.com>
2018-01-20 15:40:37 +0200

    edit my posts view

--- app/routes.js
+++ app/routes.js
@@ -531,10 +531,22 @@ module.exports = function(app, passport) {
         var user_id = req.user._id
         get_user_posts(user_id, function(result){
             res.render('posts.ejs',{
-               result : result 
+               result : result,
+               user_id : user_id 
             })
         })
     })
+
+    app.get('/all_posts',isLoggedIn, function(req, res){
+        var user_id = req.user._id
+        get_user_posts(user_id, function(result){
+            res.render('posts.ejs',{
+               result : result,
+               user_id : user_id 
+            })
+        })
+    })
+    
     app.get('/account', isLoggedIn, function(req, res){
         var user = req.user;
         res.render('account.ejs',{
